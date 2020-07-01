3147dceeb147ef87b805a80d76ec5de06673456b
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-01 15:22:59 +0700

    Thực hành Function

--- src/functions.ts
+++ src/functions.ts
@@ -1,25 +1,23 @@
 import './scss/styles.scss';
 
 function square(num: number): number {
-  return num * num;
+    return num * num;
 }
 
 console.log(square(5));
-// Output: 25
 
-// Function expression
 const squareFE = function (num: number): number {
-  return num * num;
+    return num * num;
 }
 
-// Higher-order function
+console.log(squareFE(9));
+
 function add(a: number): Function {
-  return function(b: number): number {
-    return a + b;
-  }
+    return function (b: number): number {
+        return a + b;
+    }
 }
 
 const addWith5 = add(5);
 console.log(addWith5(3));
 console.log(addWith5(15));
-
